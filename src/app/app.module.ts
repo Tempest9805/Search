181f24386a13419b27e84c78da7f41a98284a2f2
Tempest9805 <111609578+Tempest9805@users.noreply.github.com>
2024-11-12 14:08:25 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { GifsModule } from './gifs/gifs.module';
     GifsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 const GIPHY_API_KEY = 'U9uyWwp0uR1FRbnRK5Ot1S25Ax06Hq6U'
 
@@ -6,11 +8,15 @@ const GIPHY_API_KEY = 'U9uyWwp0uR1FRbnRK5Ot1S25Ax06Hq6U'
   providedIn: 'root'
 })
 export class GifsService {
+  public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
   private apikey: string = GIPHY_API_KEY;
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -22,15 +28,42 @@ export class GifsService {
     if(this._tagsHistory.includes(tag)){
       this._tagsHistory = this._tagsHistory.filter((oldtag) => oldtag !== tag)
     }
-
+    this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage() {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory))
+  }
+
+  private loadLocalStorage(){
+    if ( !localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse (localStorage.getItem('history')! );
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag( this.tagsHistory[0])
   }
 
   searchTag( tag: string ):void {
     if (tag.length === 0) return;
     this.organizeHistory(tag)
-    this._tagsHistory.unshift(tag);
-    console.log(this.tagsHistory);
+    
+    const params = new HttpParams()
+      .set('api_key', this.apikey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=U9uyWwp0uR1FRbnRK5Ot1S25Ax06Hq6U&q=Valorant&limit=100')
+    // .then(resp => resp.json())
+    // .then(data => console.log(data))
+
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, {params})
+      .subscribe( resp =>{
+        
+        this.gifList = resp.data;
+
+      });
   }
 
 }
